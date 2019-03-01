0d6e74fd2868f2e498046113b584bd52720ace2f
AlexanderNovosad <nztux@ukr.net>
2019-03-01 17:23:21 +0200

    Spring Boot Init

---
+++ src/main/java/com/api/BankruptcyRiskAssessment/BankruptcyRiskAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.api.BankruptcyRiskAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankruptcyRiskAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankruptcyRiskAssessmentApplication.class, args);
+	}
+
+}
